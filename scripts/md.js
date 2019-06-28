471b15309630c58c77d3d078e560404d850e9d3f
宁宁 <383514580@qq.com>
2019-06-28 11:04:22 +0800

    chore(build): 修改生成md的代码, 标记内部的javascript

--- scripts/md.js
+++ scripts/md.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const chalk = require('chalk');
 const json = require('../src/RULES.js');
 let content = `# any-rule 
 正则大全
@@ -6,7 +7,8 @@ https://any86.github.io/any-rule/
 `;
 json.forEach(({title, rule})=>{
     content+= `\r\n## ${title}\r\n`;
-    content+= `\`\`\`${rule}\`\`\`\r\n`;
+    content+= `\`\`\`javascript\r\n`;
+    content+= `${rule}\r\n\`\`\`\r\n`;
 });
-fs.writeFileSync('../README.md',content,'utf-8');
-// console.log(content);
+fs.writeFileSync('./README.md',content,'utf-8');
+console.log(chalk.green('生成完毕'));
