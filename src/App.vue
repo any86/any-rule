056a08ab47fe4c8b38fd5b08461e1fe88b34dfd3
宁宁 <383514580@qq.com>
2019-07-04 15:59:28 +0800

    feat: 增加显示github star数

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,7 @@
         </article>
         <header>
             <h1>正则大全</h1>
+            
             <a
                 onclick="_hmt.push(['_trackEvent', '交互', '点击github', '头部'])"
                 style="margin-left:15px;color:#000;"
@@ -37,8 +38,11 @@
                         d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
                     ></path>
                 </svg>
-                github
+                <span>github</span>
             </a>
+            
+            <iframe style="display:block;margin-top:15px;" src="https://ghbtns.com/github-btn.html?user=any86&repo=any-rule&type=star&count=true" frameborder="0" scrolling="0" width="170px" height="20px"></iframe>
+
             <input
                 ref="searchInput"
                 v-model="keyword"
@@ -292,6 +296,7 @@ main {
     header {
         padding: 15px;
         > .search-input {
+            display: block;
             margin-top: 15px;
             font-size: 16px;
             border: 1px solid #ddd;
@@ -302,6 +307,11 @@ main {
         }
         h1 {
             display: inline-block;
+        }
+        >.github{display: inline-flex;
+            >span{
+                margin-left: 5px;
+            }
         }
     }
     ul.list {
