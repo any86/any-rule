7eee2ab53040e4866a21977d142436e41bfe7942
宁宁 <383514580@qq.com>
2019-08-02 16:46:17 +0800

    合并dev

---
+++ out/test/suite/extension.test.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const assert = require("assert");
+const mocha_1 = require("mocha");
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+const vscode = require("vscode");
+// import * as myExtension from '../extension';
+suite('Extension Test Suite', () => {
+    mocha_1.before(() => {
+        vscode.window.showInformationMessage('Start all tests.');
+    });
+    test('Sample test', () => {
+        assert.equal(-1, [1, 2, 3].indexOf(5));
+        assert.equal(-1, [1, 2, 3].indexOf(0));
+    });
+});
+//# sourceMappingURL=extension.test.js.map
