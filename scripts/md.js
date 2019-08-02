d8e56fa16cbd6c78bc9a72f32fed884b58b17198
383514580 <383514580@qq.com>
2019-08-02 23:40:09 +0800

    refactor:兼容低版本vscode

--- scripts/md.js
+++ scripts/md.js
@@ -9,8 +9,9 @@ let content = `# 正则大全  ![](https://img.shields.io/badge/状态-积极更
 https://any86.github.io/any-rule/
 
 ## vscode插件
-1. 进入vscode后按**F1**.
-2. 输入"**zz**"可以看到正则列表.
+1. 在vscode的插件搜索中输入"**any-rule**"安装.
+1. 安装后进入vscode后按**F1**打开命令控制台.
+2. 输入"**zz**"后可看到正则列表.
 3. 或者**输入关键词**, 比如"手机".
 
 ![预览动画加载中...](https://camo.githubusercontent.com/1f8b23e9081ff2e3074fcaffddbfe98475ed9419/68747470733a2f2f757365722d676f6c642d63646e2e786974752e696f2f323031392f382f322f313663353136633434366430396633623f773d34323026683d32343326663d67696626733d32353537383036)
