8b38ec1521187822fb5c5f4b417e722cfb4e486b
宁宁 <383514580@qq.com>
2020-02-21 02:24:15 +0800

    feat: "zz."的对话框改为"showQuickPick", 从而支持"模糊查询"

--- src/useCompletion.ts
+++ src/useCompletion.ts
@@ -1,27 +1,37 @@
 import { ExtensionContext, CompletionItem, CompletionItemKind, languages, Disposable, workspace, window, commands, TextDocument, Position, Range, Selection, MarkdownString } from "vscode";
 import { Rule } from './interface';
 import { COMPLETION_TRIGGER_ID } from './constant';
+// import { slugify } from 'transliteration';
+
 export default function (context: ExtensionContext, RULES: Rule[]) {
-    registerCommand();
+    // commands.registerCommand('functions.insertRegex', insertRule);
+
     const disposable = languages.registerCompletionItemProvider('plaintext', {
         provideCompletionItems(document, position) {
             const linePrefix = document.lineAt(position).text.substr(0, position.character);
             if (!linePrefix.endsWith(COMPLETION_TRIGGER_ID)) return;
 
-            return RULES.map(({ rule, title, examples }) => {
-                const item = new CompletionItem(title, CompletionItemKind.Method);
-                const ruleString = String(rule);
-
-                item.insertText = ruleString;
-                item.documentation = new MarkdownString(`### 示例\r\n${examples.join(',')}`);
-                // item.filterText = '手';
-                item.command = {
-                    title: '插入正则',
-                    command: 'functions.insertRegex',
-                    arguments: [document, position, ruleString]
+            // showQuickPick
+            let index = -1;
+            window.showQuickPick(RULES.map(({ examples, title }, i) => {
+                index = i;
+                return { 
+                    label: title,
+                    // description:'例如: ' + examples[0],
+                    detail:`例如: ${examples.join(' 或 ')}`
                 };
-                return item;
-            })
+            }), {
+                placeHolder: '请输入关键词',
+                // onDidSelectItem(item){
+                //     console.log(item)
+                // }
+            }).then(text => {
+                if (!text) return
+                insertRule(document, position, '' + RULES[index].rule)
+                window.showInformationMessage('' + RULES[index].rule);
+            });
+
+            return void 0;
         },
 
         resolveCompletionItem(item) {
@@ -31,28 +41,29 @@ export default function (context: ExtensionContext, RULES: Rule[]) {
     context.subscriptions.push(disposable);
 }
 
-function registerCommand() {
-    commands.registerCommand('functions.insertRegex', (document: TextDocument, position: Position, ruleString: string) => {
-        const editor = window.activeTextEditor;
-        if (void 0 === editor) return;
-        editor.edit(editBuilder => {
-            const line = document.lineAt(position);
-            // 起始
-            const startPostion = new Position(line.lineNumber, line.text.indexOf(COMPLETION_TRIGGER_ID));
 
-            // 结束
-            const endPostion = new Position(line.lineNumber, startPostion.character + ruleString.length + COMPLETION_TRIGGER_ID.length);
+function insertRule(document: TextDocument, position: Position, ruleString: string) {
+    const editor = window.activeTextEditor;
+    if (void 0 === editor) return;
+    editor.edit(editBuilder => {
+        const line = document.lineAt(position);
+        // 起始
+        const startPostion = new Position(line.lineNumber, line.text.indexOf(COMPLETION_TRIGGER_ID));
 
-            // window.showInformationMessage(
-            //     '' + startPostion.character
-            //     , ''+endPostion.character
-            //     );
+        // 结束
+        const endPostion = new Position(line.lineNumber, startPostion.character + ruleString.length + COMPLETION_TRIGGER_ID.length);
+        window.showInformationMessage('asdad')
 
-            editBuilder.replace(new Range(startPostion, endPostion), ruleString);
+        // window.showInformationMessage(
+        //     '' + startPostion.character
+        //     , ''+endPostion.character
+        //     );
 
-            setTimeout(() => {
-                editor.selection = new Selection(startPostion, endPostion);
-            }, 0);
-        });
+        editBuilder.replace(new Range(startPostion, endPostion), ruleString);
+
+        setTimeout(() => {
+            editor.selection = new Selection(startPostion, endPostion);
+        }, 0);
     });
 }
+
