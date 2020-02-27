37657b15d24e553a3b1a357f16706c25e4bf2df9
宁宁 <383514580@qq.com>
2020-02-27 23:26:17 +0800

    refactor: 修改文案

--- src/showResultMessage.ts
+++ src/showResultMessage.ts
@@ -2,8 +2,8 @@ import { extensions, window, version, env, Uri, languages } from 'vscode';
 import { getCodeLanguage, getExtensionVersion } from './shared'
 import insertLog from './insertLog';
 
-const BUTTON_FEEDBACK = '🚀反馈问题';
-const BUTTON_DIAGRAMMATIZE = '🦕图解正则';
+const BUTTON_FEEDBACK = '✋意见反馈';
+const BUTTON_DIAGRAMMATIZE = '🤖图解正则';
 const BUTTON_CANCEL = '关闭';
 
 export default function (title: string, rule: string): void {
