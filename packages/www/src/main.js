6674643359c0e7b2b5a94e2912cea47f45b20fc2
any86 <383514580@qq.com>
2020-04-03 09:03:20 +0800

    wip: 建设pwa

--- packages/www/src/main.js
+++ packages/www/src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('./sw.js');
+    });
+}
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
