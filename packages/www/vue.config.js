152c015f7d669fc86fa30338aaaf9ae3d0352b9d
Russell <383514580@qq.com>
2022-09-01 10:02:16 +0800

    chore: 修改demo的打包路径

--- packages/www/vue.config.js
+++ packages/www/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/any-rule/'
-  : '/'
+  // publicPath: process.env.NODE_ENV === 'production'
+  // ? '/any-rule/'
+  // : '/'
 }
